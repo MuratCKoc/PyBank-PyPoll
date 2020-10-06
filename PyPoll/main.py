4bc1ba6d65f73047697d95b4233c4fa8e0d8ca34
MuratCKOC <muratcagrikoc@gmail.com>
2020-10-05 20:56:51 -0400

    read test csv and print

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,14 +1,28 @@
 import csv
 import os
 
+vote_csv = os.path.join("Resources","test.csv")
 # Declare global variables
+voteDict = {}
+voteCounter = 0
 # Function to create new record
 # Function to increment vote count
-# Function to display and write to a file
+# Display and write to a file
+# Percentage calculator function
+
 # Main Loop
 # Read file
-# Check if candidate exist
-# If not exist create a new record
-# If candidate exists then increment the vote count
+with open(vote_csv, "r") as csvfile:
+
+    csvreader = csv.reader(csvfile,delimiter=",")
+    # Skip the header row
+    header = next(csvreader)
+    
+    for row in csvreader:
+        print (row)
+
+    # Check if candidate exist
+    # If not exist create a new record
+    # If candidate exists then increment the vote count
 
 
