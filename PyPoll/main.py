6bc1082bd7c4f539354ef41c701c10519755a657
MuratCKOC <muratcagrikoc@gmail.com>
2020-10-05 20:37:24 -0400

    main

---
+++ PyPoll/main.py
@@ -0,0 +1,14 @@
+import csv
+import os
+
+# Declare global variables
+# Function to create new record
+# Function to increment vote count
+# Function to display and write to a file
+# Main Loop
+# Read file
+# Check if candidate exist
+# If not exist create a new record
+# If candidate exists then increment the vote count
+
+
