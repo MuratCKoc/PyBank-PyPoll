458539a409098b932f62d6041c736fc32eabdf5c
MuratCKOC <muratcagrikoc@gmail.com>
2020-10-05 23:34:01 -0400

    percentage calculated, zipped and sorted

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,15 +1,22 @@
 import csv
 import os
+import operator
 
 vote_csv = os.path.join("Resources","test.csv")
 # Declare global variables
 voteDict = {}
-voteCounter = 0
-founde = 0
-# Function to create new record
-# Function to increment vote count
+totalVotes = 0
+
+
 # Display and write to a file
 # Percentage calculator function
+
+# Sort the dictionary
+def sortDict(e):
+    return 
+
+def byVoteCount(elem):
+    return elem[1]
 
 # Main Loop
 # Read file
@@ -23,22 +30,37 @@ with open(vote_csv, "r") as csvfile:
         print (row)
 
         # Increment the vote counter
-        voteCounter += 1
+        totalVotes += 1
         # Check if candidate exist
         if row[2] in voteDict :
             voteDict[row[2]] += 1
         # If not exist create a new record
         else:
             voteDict[row[2]] = 1
-    print (voteCounter)
+    print (totalVotes)
     print (voteDict)
+
+    # Sort the dictionary by voteCount 
+    
+    # Calculate Percentage 
     voteCandidates = voteDict.keys()
     voteCounts = voteDict.values()
-    print (f'keys {voteCandidates}')
-    print (f'values {voteCounts}')
     votePercentage = []
     for val in voteCounts:
-        votePercentage.append(round((val/voteCounter)*100,3))
+        votePercentage.append(round((val/totalVotes)*100,3))
     print(f'Percentage {votePercentage}')
-    resultsDict = zip(voteCandidates, voteCounts, votePercentage)
-    print (tuple(resultsDict))
+
+    # Zip list of values and sort them by number of votes
+    l = list(zip(voteCandidates,voteCounts,votePercentage))
+    l.sort(key=byVoteCount,reverse=True)
+    print (f'list: -- {l}')
+
+    #new_dict = {k: dict(zip(voteCandidates,v)) for k, v in zip(voteCandidates, zip(voteCounts,votePercentage))}
+    #print (new_dict)
+    # sorted_dict = sorted(new_dict.items(), key=lambda kv: kv[1], reverse=True)
+    # Destructure dictionary
+    
+    
+    print(f'keys {voteCandidates}')
+    print(f'values {voteCounts}')
+    #print (x)
