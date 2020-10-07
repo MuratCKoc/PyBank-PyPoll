2bd4cdacdef4cf80489653defd18bb832dd4e4e3
MuratCKOC <muratcagrikoc@gmail.com>
2020-10-07 08:39:58 -0400

    Challenge completed, cleaned, commented

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,18 +4,11 @@ import operator
 
 # Filepath for input and output
 write_txt = os.path.join("analysis","results.txt")
-
 vote_csv = os.path.join("Resources","election_data.csv")
 
 # Declare global variables
 voteDict = {}
 totalVotes = 0
-
-
-
-# Sort the dictionary
-def sortDict(e):
-    return 
 
 def byVoteCount(elem):
     return elem[1]
@@ -25,12 +18,14 @@ def printResults(l):
     print("-------------------------")
     print(f"Total Votes: {totalVotes}")
     print("-------------------------")
+    # Print results with the help of loop
     for iter in l:
         print(f"{iter[0]}: {iter[2]}% ({iter[1]})")
     print("-------------------------")
     print(f'Winner: {l[0][0]}')
     print("-------------------------")
 
+# Print results to txt file
 def exportReport():
     with open(write_txt,"w") as fileX:
         
@@ -44,19 +39,20 @@ def exportReport():
         fileX.write(f'Winner: {l[0][0]}\n')
         fileX.write("-------------------------")
 
-
 # Percentage calculator function
+def get_Percentage(val):
+    return round((val/totalVotes)*100,3)
 
-
-# Main Loop
 # Read file
 with open(vote_csv, "r") as csvfile:
-
     csvreader = csv.reader(csvfile,delimiter=",")
-    # Skip the header row
+
+    # Store the header row
     header = next(csvreader)
     
+    # Main Loop
     for row in csvreader:
+
         # Increment the vote counter
         totalVotes += 1
         # Check if candidate exist
@@ -66,17 +62,19 @@ with open(vote_csv, "r") as csvfile:
         else:
             voteDict[row[2]] = 1
 
-    # Sort the dictionary by voteCount 
-    
-    # Calculate Percentage 
+    # Destructure and format the data 
     voteCandidates = voteDict.keys()
     voteCounts = voteDict.values()
+
+    # Calculate Percentage, and populate list
     votePercentage = []
     for val in voteCounts:
-        votePercentage.append(round((val/totalVotes)*100,3))
+        votePercentage.append(get_Percentage(val))
 
     # Zip list of values and sort them by number of votes
     l = list(zip(voteCandidates,voteCounts,votePercentage))
+    
+    # Sort dictionary by voteCount
     l.sort(key=byVoteCount,reverse=True)
 
     # Print Results
