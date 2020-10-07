e0be37062200573f7d94c77a9cedc33e5f620676
MuratCKOC <muratcagrikoc@gmail.com>
2020-10-07 08:28:31 -0400

    Challange completed, cleaned-up, commented and ready to get mark

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,61 +1,95 @@
 import csv
+import os
 
+write_txt = os.path.join("analysis","results.txt")
+read_csv = os.path.join("Resources","budget_data.csv")
 
-#def main():
-with open("Resources/budget_data.csv") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    lineCounter = 0
-    totalAmount = 0
-    oldAmount = 0
-    newAmount = 0
-    monthlyChange = 0
-    averageChange = 0
-    greatestIncrease = {
-        "date": "",
-        "amount": 0
-    }
-    greatestDecrease = {
-        "date": "",
-        "amount": 0
-    }
-    for row in csv_reader:
-        if lineCounter == 0:
-            print(f'Column names are {", ".join(row)}')
-            lineCounter += 1
-        else:
-            print(f'\t{row[0]} --- {row[1]}')
-            lineCounter += 1
+lineCounter = 0
+totalAmount = 0
+oldAmount = 0
+newAmount = 0
+monthlyChange = 0
+averageChange = 0
 
-        # TotalAmount    
-        if row[1] != "Profit/Losses":
-            totalAmount += int(row[1])
-            newAmount = int(row[1])
+# Dictionaries to keep records of max and min
+greatestIncrease = {
+    "date": "",
+    "amount": 0
+}
+greatestDecrease = {
+    "date": "",
+    "amount": 0
+}
+# List to keep monthly changes
+aChange = []
 
-            #if newAmount != oldAmount :
-            averageChange += monthlyChange
-            monthlyChange = newAmount-oldAmount
-            print(f'\t{averageChange} , :::: {monthlyChange}')
-            if monthlyChange > greatestIncrease.get("amount"):
-                greatestIncrease["date"] = row[0]
-                greatestIncrease["amount"] = monthlyChange
-                
-            if monthlyChange < greatestDecrease.get("amount"):
-                greatestDecrease["date"] = row[0]
-                greatestDecrease["amount"] = monthlyChange
+def findAverage(l):
+    return (sum(l)/len(l))
 
-            oldAmount = int(row[1])
-
-     # display(lineCounter)
+# Displat results to console
+def printResults():
     print(f'Financial Analysis')
     print(f'------------------')
-    print(f'Total Months: {lineCounter-1}')
+    print(f'Total Months: {lineCounter}')
     print(f'Total:\t ${totalAmount}')
-    #print(f'Average Change: ${averageChange/(lineCounter-2)}')
-    print(f'Greatest Increase in Profits: ${greatestIncrease["date"]} (${greatestIncrease["amount"]})')
-    print(f'Greatest Decrease in Profits: ${greatestDecrease["date"]} (${greatestDecrease["amount"]})')
+    print(f'Average Change: ${round(findAverage(aChange),2)}')
+    print(
+        f'Greatest Increase in Profits: ${greatestIncrease["date"]} (${greatestIncrease["amount"]})')
+    print(
+        f'Greatest Decrease in Profits: ${greatestDecrease["date"]} (${greatestDecrease["amount"]})')
     print(f'Processed {lineCounter} rows.')
 
-# def display(lineCounter):
-#     print(f'Financial Analysis')
-#     print(f'Total Months: {lineCounter-1}')
-#     print(f'Processed {lineCounter} months.')
+# Export to txt file
+def exportReport():
+    with open(write_txt,"w") as fileX:
+        fileX.write(f'Financial Analysis\n')
+        fileX.write(f'------------------\n')
+        fileX.write(f'Total Months: {lineCounter}\n')
+        fileX.write(f'Total:\t ${totalAmount}\n')
+        fileX.write(f'Average Change: ${round(findAverage(aChange),2)}\n')
+        fileX.write(
+        f'Greatest Increase in Profits: ${greatestIncrease["date"]} (${greatestIncrease["amount"]})\n')
+        fileX.write(
+        f'Greatest Decrease in Profits: ${greatestDecrease["date"]} (${greatestDecrease["amount"]})\n')
+        
+# def main():
+with open(read_csv) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    # Skip the first row
+    header = next(csv_reader)
+
+    # Process the first record to assign relevant variables for Change calculation
+    remover = next(csv_reader)
+    oldAmount=int(remover[1])
+    totalAmount = int(remover[1])
+    lineCounter = 1
+
+    for row in csv_reader:
+
+        # Month (Record) Counter
+        lineCounter += 1
+
+        # TotalAmount
+        totalAmount += int(row[1])
+        
+        # Calculate Average change
+        newAmount = int(row[1])
+        monthlyChange = newAmount-oldAmount
+        oldAmount = int(row[1])
+        # Add new value to the list
+        aChange.append(monthlyChange)
+        
+        # Find Greatest Increase
+        if monthlyChange > greatestIncrease.get("amount"):
+            greatestIncrease["date"] = row[0]
+            greatestIncrease["amount"] = monthlyChange
+
+        # Find Greatest Decrease
+        if monthlyChange < greatestDecrease.get("amount"):
+            greatestDecrease["date"] = row[0]
+            greatestDecrease["amount"] = monthlyChange
+
+    #Print and Export the results       
+    printResults()
+    exportReport()
